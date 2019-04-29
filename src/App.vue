c7c86a8eb8129a774ff452154aa00d0ac31df304
snychka <stefannychka@gmail.com>
2019-04-29 14:03:16 -0600

    confirming syntax error has no output from check my work

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello-world></hello-world>
-  </div>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <book-list></book-list>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import BookList from './components/BookList'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
-};
+    name: 'app',
+    components: {
+        BookList,
+    },
+}
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
